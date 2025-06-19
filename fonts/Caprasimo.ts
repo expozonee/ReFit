889cb8b62293fbf9a4202aa4a13813ab0545e82b
Ibrahem <boss.abrahem@hotmail.com>
2025-06-19 21:45:27 +0300

    created for each font its own file

---
+++ fonts/Caprasimo.ts
@@ -0,0 +1,8 @@
+import { Caprasimo as CaprasimoFont } from "next/font/google";
+
+export const caprasimo = CaprasimoFont({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-caprasimo",
+  display: "swap",
+});
---
+++ fonts/Parkinsans.ts
@@ -0,0 +1,8 @@
+import { Parkinsans } from "next/font/google";
+
+export const parkinsans = Parkinsans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-parkinsans",
+  display: "swap",
+});
